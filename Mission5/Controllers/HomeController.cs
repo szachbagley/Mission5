f1d3ec60ef92b14c372b5d3962d387a6a8b4c123
Zach Bagley <szachbagley@gmail.com>
2024-02-09 10:28:53 -0700

    finished

--- Mission5/Controllers/HomeController.cs
+++ Mission5/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace Mission5.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Pricing()
         {
             return View();
         }
--- Mission5/wwwroot/js/site.js
+++ Mission5/wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+    $('#calculatorForm').on('submit', function (e) {
+        e.preventDefault();
+        var hours = $('#hours').val();
+        var rate = $('#rate').val();
+
+        // Check if the hours entered is a positive number
+        if (hours <= 0) {
+            alert("Please enter a positive number for hours.");
+            return;
+        }
+        else {
+            var total = hours * rate;
+            $('#total').val(total);
+        }
+    });
+});
